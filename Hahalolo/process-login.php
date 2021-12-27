b229b1d40e57acdc0b6a636f69b7fda8408a7c48
14_1951064145_Nguyễn Công Giang <94435725+ncgiang21@users.noreply.github.com>
2021-12-28 02:43:04 +0700

    Revert "Giang"

---
+++ Hahalolo/process-login.php
@@ -0,0 +1,42 @@
+<?php
+    //Tạo season: mặc định mỗi phiên 24p
+    session_start();
+    //login.php truyền dữ liệu sang: nhận dữ liệu gửi sang
+    if(isset($_POST['btnLogin'])){
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+        //Ở đây kiểm tra người dùng đã đăng nhập chưa
+
+        //Kết nối database
+        $conn = mysqli_connect('localhost','root','','hahalolo');
+        if(!$conn){
+            die("Kết nối thất bại. Vui lòng kiểm tra lại thông tin máy chủ.");
+        }
+
+        //Thực hiện truy vấn
+        $sql = "SELECT * FROM users WHERE email='$email' AND password= '$pass'";
+        //Ở đây còn có các vấn đề về tính hợp lệ dữ liệu vào form
+        //Nghiêm trọng: lỗi SQL injection
+
+
+        $result = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result) > 0){
+            $row = mysqli_fetch_array($result);
+            if ($row['status'] == 1){
+                $_SESSION['isLoginOK'] = $email;
+                header("location: index.php"); //Chuyển hướng tới trang chủ
+            }else{
+                $error = "Tài khoản của bạn chưa được kích hoạt, vui lòng kiểm tra lại email";
+                header("location: login.php?error=$error");
+            }
+        }else{
+            $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
+            header("location: login.php?error=$error"); //Chuyển hướng, hiển thị lỗi
+        }
+        
+        //Đóng kết nối
+        mysqli_close($conn);
+    }else{
+        header("location: login.php");
+    }
+?>
